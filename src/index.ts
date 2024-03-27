c9c640592d06cb0f47d294a702157e7709cecc2c
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-03-28 00:08:13 +0100

    feat(Root): docstring added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { UserOS } from './types/user-os'
 // Exports
 export { UserOS } from './types/user-os'
 
+/**
+ * Returns the operating system of the user based on the user agent.
+
+ * @returns {UserOS} The operating system of the user.
+ */
 export function getUserOS(): UserOS {
   if (navigator.userAgent.includes('Win')) return UserOS.Windows
   if (navigator.userAgent.includes('iPhone OS')) return UserOS.iOS
